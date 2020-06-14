45810c0e70a52b1fd2079568510efdc5aded864b
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-14 22:00:10 +0200

    feat(routes): add auth middlewares

--- server/src/router.js
+++ server/src/router.js
@@ -3,7 +3,17 @@ import {
   getWeather
 } from './controllers/address.js'
 
+import {
+  register,
+  login
+} from './controllers/auth.js'
+
 export default ({ app, cache }) => {
+  // public
+  app.post('/auth/register', register)
+  app.post('/auth/login', login)
+
+  // under auth...
   app.get('/address/is_valid', cache, isValid)
   app.get('/address/weather', cache, getWeather)
 }
