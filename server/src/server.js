610f1ee82d59673a0576be0bb41c8b26edc43c44
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-14 12:46:09 +0200

    feat(server): add mongo connector

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import router from './router.js'
 import bodyParser from 'body-parser'
+import mongo from './mongo.js'
+
+await mongo()
 
 const app = express()
 app.use(bodyParser.json())
