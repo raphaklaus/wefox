17baddf1555c5d8654ffde3b6005c8989e9339f1
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-14 14:22:31 +0200

    feat(controller): add requestLog service to controller

--- server/src/controllers/address.js
+++ server/src/controllers/address.js
@@ -1,9 +1,11 @@
 import googleMaps from '../services/googleMaps.js'
 import mapsValidation from '../services/mapsValidation.js'
 import weather from '../services/openWeather.js'
+import requestLogService from '../services/requestLog.js'
 
 export const isValid = async (req, res) => {
   const mapsRespose = await googleMaps(req.body)
+  await requestLogService.create()
   return res.json({
     valid: mapsValidation(mapsRespose)
   })
