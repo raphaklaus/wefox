fe4fe65fe03c51850728c769d93167df3ec90d7e
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-14 12:45:01 +0200

    feat(mongo): add mongodb connector

---
+++ server/src/mongo.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+export default async () => {
+  console.log(`mongodb://${process.env.MONGO_HOST}/${process.env.DB_NAME}`)
+
+  try {
+    await mongoose.connect(`mongodb://${process.env.MONGO_HOST}/${process.env.DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
